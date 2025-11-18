5907cedcb7960e5e8bfa9a82eb3167783ce38220
ACK0007 <ahmetcevdetkaya@yahoo.com>
2025-11-17 21:07:39 -0500

    Implemented argparse

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from Faster_R_CNN import Faster_R_CNN
 from evaluation import evaluate
 from train import train
+import argparse
 
 
-def main():
+def main(args: argparse.Namespace):
     in_channels = 3
     num_classes = 80
-    data_source = '/Users/ahmet/Desktop/python/PyTorch/COCO'
-    batch_size = 1
-    epochs = 10
+    data_source = args.data_source
+    batch_size = args.batch_size
+    epochs = args.epochs
     
     model = Faster_R_CNN(in_channels, num_classes)
-    train(model, data_source, batch_size=batch_size, epochs=epochs)
+    train(model, data_source, batch_size=batch_size, epochs=epochs, lr=args.lr)
     evaluate(model, data_source)
     
     
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-data_source", type=str, default='/Users/ahmet/Desktop/python/PyTorch/COCO')
+    parser.add_argument("-lf", type=float, default=1e-3)
+    parser.add_argument("-batch_size", type=int, default=1)
+    parser.add_argument("-epochs", type=float, default=10)
+    args = parser.parse_args()
+    main(args)
