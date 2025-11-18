0e9d62ec8d1840b2e9168c4dbd148f007ae3e11b
Richard Tang <130111909+CodeKnight314@users.noreply.github.com>
2025-11-17 21:05:47 -0500

    Organize downloaded files into data directory
    Create a data directory and move downloaded files into it, then unzip the files.

--- setup.sh
+++ setup.sh
@@ -6,7 +6,17 @@ wget http://images.cocodataset.org/zips/val2017.zip
 wget http://images.cocodataset.org/zips/test2017.zip
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 
+mkdir data
+
+mv train2017.zip data/train2017.zip
+mv val2017.zip data/val2017.zip
+mv test2017.zip data/test2017.zip
+mv annotations_trainval2017.zip data/annotations_trainval2017.zip
+
+cd data/
+
 unzip train2017.zip
 unzip val2017.zip 
 unzip test2017.zip 
 unzip annotations_trainval2017.zip
+
